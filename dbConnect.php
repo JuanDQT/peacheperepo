609dbe9d09df106363cabc06d325e7d6ae3b4c8f
Juan Daniel <juandaniel@MacBook-Pro-de-Juan.local>
2017-01-27 19:12:04 +0100

    Sapex2

---
+++ dbConnect.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: juandaniel
+ * Date: 27/1/17
+ * Time: 19:08
+ */
+
+$username = 'root';
+$password = 'root';
+
+$db = new PDO('mysql:host=localhost;dbname=codeigniter;charset=utf8mb4', $username, $password);
